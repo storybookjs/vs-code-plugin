5298a02fb1be3c75dad63785ce13f980b2439eec
Keith Cagney <keithrcagney@gmail.com>
2019-12-19 20:00:35 -0500

    Pseudo code for file system traversal

--- src/extension.ts
+++ src/extension.ts
@@ -1,13 +1,20 @@
 import * as vscode from 'vscode';
-// import * as express from 'express';
+import * as express from 'express';
+
+//port should be variable to listen for action in the user's active terminal
+const PORT = 8509;
+const server = express();
 
 // import MainUI from './toolbars/main_ui.ts';
-const PORT = 6006;
+// import {server, PORT} from './server/server';
 
 export function activate(context: vscode.ExtensionContext) {
-	// const app = express();
-	// app.get('/', (_req, res) => res.status(200).send('Connected'));
-	// app.listen(PORT);
+	server.get('/', (req : Object, res : Object) => {
+		vscode.window.showInformationMessage('Aesop server online');
+		res.end();
+	});
+
+	server.listen(PORT);
 
 	//create disposable variable type, registers awaken command & opens webview
 	let disposable = vscode.commands.registerCommand('extension.aesopAwaken', () => {
@@ -18,15 +25,57 @@ export function activate(context: vscode.ExtensionContext) {
 			{
 				enableScripts: true,
 				portMapping: [
-					//extensionHostPort should be variable to enable remote machine access
+					//should extensionHostPort be variable to enable remote machine access?
+					//PORT is presently undefined
+					//how to use express with a d.ts file
 					{ webviewPort: PORT, extensionHostPort: PORT }
 				],
-				//look in to workspace file system access
+				// (!) look in to workspace file system access
 				localResourceRoots: [vscode.Uri.file(context.extensionPath)]
 			}
 		);
 
-		panel.webview.html =
+		//{
+		//also register this command at startup to crawl the file path
+		// ${vscode.commands.executeCommand('extension.getStories')}
+		// vscode.window.showInformationMessage('Aesop is reading from your Storybook.');
+		//}
+
+		//access the first opened folder of the workspace array
+		//a potentially problematic assumption in multi-folder workspaces
+		const rootPath = vscode.workspace.workspaceFolders[0];
+
+		//define a path to SB webpack bundle outputs (in user workspace /node_modules/ folder)
+		const distGlob = new vscode.RelativePattern(rootPath, "*/node_modules/@storybook/core/dist/public/");
+
+		//instantiate a watcher to listen for fs path changes (e.g. file creation/update)
+		//bools = options for ignoreCreateEvents?, ignoreChangeEvents?, ignoreDeleteEvents?
+		const observer = vscode.workspace.createFileSystemWatcher(distGlob, false, false, false);
+
+		//extract the index.html file that outputs into SB's preview pane
+		const htmlGlob = new vscode.RelativePattern(rootPath, "*/node_modules/@storybook/core/dist/public/*.html");
+		//extract the necessary bundle scripts to leverage in-app dependencies
+		const scriptGlob = new vscode.RelativePattern(rootPath, "*/node_modules/@storybook/core/dist/public/*.js");
+		
+		//do we need to resolve the Storybook UI script from the /dll/ folder?
+		//if extract methods above fail, determine logic to parse out HTML/.js scripts (index 0?);
+
+		//retrieve files with findFiles/relativeFilePath
+		const arrayOfScripts = vscode.workspace.findFiles(distGlob, null, 100);
+
+		//dev check: have we successfully pulled down script files?
+		//if so, should we then store them locally, or is there no point?
+		if (arrayOfScripts !== undefined){
+			vscode.window.showInformationMessage("Hey, dog: " + arrayOfScripts);
+		}
+
+		//use retrieved info to fill out our HTML template inside the webview
+		//figure out how to use a tsx rule in webpack
+		//signal to babel to interpret this block as tsx, e.g.
+
+		//something like: @ tsx babel// (to determine syntax)
+
+		panel.webview.html = 
 		`<!DOCTYPE html>
 		<html lang="en">
 		<head>
@@ -34,9 +83,9 @@ export function activate(context: vscode.ExtensionContext) {
 				<meta name="viewport" content="width=device-width, initial-scale=1.0">
 				<title>Aesop</title>
 				<style>
-				html { width: 100%; height: 100%; min-height: 100%; display: flex; }
-				body { flex: 1; display: flex; }
-				iframe { flex: 1; border: none; background: white; }
+				html { width: 100%, height: 100%, min-height: 100%; display: flex; padding: 0, margin: 0}
+				body { flex: 1; display: inline-flex; width: 100%, justify-content: center}
+				iframe { flex-fl: 1; border: none; background: white; width: 50%}
 		</style>
 		</head>
 		<body>
@@ -46,22 +95,21 @@ export function activate(context: vscode.ExtensionContext) {
 						<button></button>
 						<button></button>
 					</div>
-					${vscode.commands.executeCommand('extension.getStories')}
 				</nav>
-				<iframe src="http://localhost:${PORT}"></iframe>
-				<img src="https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif" width="300" />
+				<iframe src="http://localhost:${PORT}">
+				<script>${arrayOfScripts}</script>
+				</iframe>
 		</body>
 		</html>`;
 
-		console.log('Aesop is reading from your Storybook.');
 		vscode.window.showInformationMessage(`Aesop is ready to chronicle your stories!\n\nPlease use a command to begin: ${vscode.commands.getCommands(true)}`);
 	});
 
 	//subscribe this extension to the disposable
 	context.subscriptions.push(disposable);
 
-	//breaking up subscription pushes to register each command sequentially allows us
-	//to overwrite the disposable, avoiding the creation of unnecessary variables
+	//break up subscription pushes, and register each command sequentially.
+	//this lets us overwrite "disposable," avoiding creation of unnecessary variables
 	disposable = vscode.commands.registerCommand('extension.tollTheHour', () => {
 		const nowadays = new Date(Date.now()).toTimeString();
 		vscode.window.showInformationMessage(`The hour tolls: ${nowadays}`);
@@ -75,25 +123,6 @@ export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(disposable);
 }
 
-// let mediaPath = vscode.Uri.file(path.join(context.extensionPath, 'media')).with({
-// 	scheme: "vscode-resource"
-// }).toString() + '/';
-
-// webview.html = `
-// <base href="${mediaPath}">
-// <img src="vscode.png"/>    
-// `;
-
-// webview.onRequestMade((request) => {
-//   if (request.uri.scheme == 'media') {
-//      request.uri.scheme = 'vscode-resource';
-//      request.uri.path = path.join(extensionPath, request.uri.path);
-//   }
-//   return request;
-// });
-
-// this method is called when your extension is deactivated
 export function deactivate(context: vscode.ExtensionContext) {
-		//this message says goodbye to the user when closing Aesop
 		vscode.window.showInformationMessage(`Aesop slumbers. Thank you for chronicling your Storybook with Aesop.`);
 }
