2608e6edf899d17db9ad2edace6da1d488b3c1e5
Arlo Guiwo <arloguiwo@gmail.com>
2020-05-21 11:55:42 -0400

    refactored findLocationViaNetstat and startStorybook functions into smaller, more modular functions.

--- src/processes/process.service.ts
+++ src/processes/process.service.ts
@@ -48,16 +48,25 @@ class ProcessService {
             netstatCommand.cmd,
             netstatCommand.args
         );
-        const grepProcess = child_process.spawnSync(
+        const searchProcess = child_process.spawnSync(
             searchCommand.cmd,
             [processPid],
             { input: netStatProcess.stdout, encoding: "utf-8" }
         );
 
-        const data = grepProcess.stdout;
+        this.port = this.netstatPortHelper(searchProcess)
 
-        const parts = data.split(/\s/).filter(String);
+        this.aesopEmitter.emit('create_webview', this.port);
 
+        logger.write(
+            `Found Storybook location via Netstat`,
+            this.fileName,
+            "locationViaNetstat/grepProcess"
+        );
+    }
+
+    private netstatPortHelper({ stdout }): number {
+        const parts = stdout.split(/\s/).filter(String);
         const partIndex = (platform === 'win32') ? 1 : 3;
         const localAddress = parts[partIndex]
 
@@ -69,78 +78,25 @@ class ProcessService {
             portStr = localAddress[i] + portStr;
         }
 
-        this.port = parseInt(portStr);
-        this.aesopEmitter.emit('create_webview', this.port);
-
-        logger.write(
-            `Found Storybook location via Netstat`,
-            this.fileName,
-            "locationViaNetstat/grepProcess"
-        );
+        const port = parseInt(portStr);
+        return port
     }
 
-    //MAYBE separate this function?
-    //possibly break it down as well into:
-    //grab json script
-    //start storybook
+    public startStorybook() {
+        logger.write('Starting Storybook for you!', this.fileName, 'startStorybook')
 
-    startStorybook() {
-        let data: Buffer;
-        logger.write(
-            "Starting Storybook for you!",
-            this.fileName,
-            "startStorybook"
-        );
-
-        try {
-            data = fs.readFileSync(path.join(this.rootDir, "package.json"));
-            logger.write(
-                "Obtained data from package.json",
-                this.fileName,
-                "startStorybook"
-            );
-        } catch (err) {
-            this.vscode.window.showErrorMessage(
-                `Aesop is attempting to read ${this.rootDir}. Is there a package.json file here?`
-            );
-            this.statusText.dispose();
-            return false;
+        const { childProcessArguments, childProcessCommand } = (platform === 'win32') ? this.createSbWinConfig() : this.createSbConfig();
+    const runSb = child_process.spawn(
+        childProcessCommand,
+        childProcessArguments,
+        {
+            cwd: this.rootDir,
+            env: process.env,
+            windowsHide: false,
+            windowsVerbatimArguments: true,
+            stdio: [null, null, 'ignore']
         }
-
-        this.statusText.text = `Checking package.json...`;
-
-        //check if the user has custom configurations for starting storybook
-
-        let packageJSON = JSON.parse(data.toString());
-        let storybookScript = packageJSON.scripts.storybook;
-        let retrievedScriptArray = storybookScript.split(" ");
-
-        //older Windows systems support here: check platform, change process command accordingly
-
-        const sbCLI = "./node_modules/.bin/start-storybook";
-        const sbStartIndex = retrievedScriptArray.indexOf("start-storybook");
-        retrievedScriptArray[sbStartIndex] = sbCLI;
-        retrievedScriptArray.push("--ci");
-
-        //launch storybook using a child process
-        const childProcessArguments =
-            platform === "win32" ? ["run", "storybook"] : retrievedScriptArray;
-        const childProcessCommand = platform === "win32" ? "npm.cmd" : "node";
-
-        const runSb = child_process.spawn(
-            childProcessCommand,
-            childProcessArguments,
-            {
-                cwd: this.rootDir,
-                env: process.env,
-                windowsHide: false,
-                windowsVerbatimArguments: true,
-            }
-        );
-
-        this.statusText.text = `Done looking. Aesop will now launch Storybook in the background.`;
-
-        runSb.stdout.setEncoding("utf8");
+    );
 
         let counter = 0;
 
@@ -150,25 +106,17 @@ class ProcessService {
         let callback = (data) => {
             let str = data.toString().split(" ");
             counter += 1;
-            logger.write(
-                `This is data: ${data.toString()}`,
-                this.fileName,
-                "startStorybook/runSb"
-            );
+            logger.write(`This is data: ${data.toString()}`, this.fileName, 'startStorybook/runSb')
 
             if (counter >= 2) {
                 for (let i = 165; i < str.length; i += 1) {
-                    if (str[i].includes("localhost")) {
+                    if (str[i].includes('localhost')) {
                         const path = str[i];
-                        const regExp = /[^0-9]/g;
-                        this.port = path.replace(regExp, "");
+                        const regExp = (/[^0-9]/g);
+                        this.port = (path.replace(regExp, ""));
 
-                        logger.write(
-                            "Found instance in stdout that includes localhost",
-                            this.fileName,
-                            "startStorybook/runSb"
-                        );
-                        this.aesopEmitter.emit("create_webview", this.port);
+                        logger.write('Found instance in stdout that includes localhost', this.fileName, 'startStorybook/runSb')
+                        this.aesopEmitter.emit('create_webview', this.port);
                         return true;
                     }
                 }
@@ -176,23 +124,63 @@ class ProcessService {
         };
         callback = callback.bind(this);
 
-        runSb.stdout.on("data", callback);
 
-        runSb.stderr.on("data", (data) => {
-            console.error(`stderr: ${data}`);
-            logger.write(
-                `This is stderr: ${data.toString()}`,
-                this.fileName,
-                "startStorybook/runSb"
-            );
-            // process.exit(1);
-        });
-
-        //make sure the child process is terminated on process exit
-        runSb.on("close", (code) => {
-            console.log(`child process exited with code ${code}`);
-        });
+        runSb.stdout.on('data', callback)
+    runSb.on("close", (code) => {
+        console.log(`child process exited with code ${code}`);
+    });
     }
+
+    //creates shell commands to spin up storybook, depending on your platform
+    private createSbWinConfig() {
+        const config = {
+            childProcessArguments: ['run', 'storybook'],
+            childProcessCommand: 'npm.cmd'
+        }
+        return config;
+    }
+
+    private createSbConfig() {
+        const config = {
+            childProcessArguments: [],
+            childProcessCommand: ''
+        }
+
+        //older Windows systems support here: check platform, change process command accordingly
+        const retrievedScriptArray = this.obtainSbScript();
+
+        const sbCLI = './node_modules/.bin/start-storybook'
+        const sbStartIndex = retrievedScriptArray.indexOf('start-storybook')
+        retrievedScriptArray[sbStartIndex] = sbCLI;
+        retrievedScriptArray.push('--ci')
+
+        //launch storybook using a child process
+        config.childProcessArguments = retrievedScriptArray;
+        config.childProcessCommand = 'node';
+
+        return config;
+    }
+
+    private obtainSbScript() {
+        let configData: Buffer;
+
+        try {
+            configData = fs.readFileSync(path.join(this.rootDir, 'package.json'));
+            logger.write('Obtained data from package.json', this.fileName, 'startStorybook');
+        } catch (err) {
+            this.vscode.window.showErrorMessage(`Aesop is attempting to read ${this.rootDir}. Is there a package.json file here?`);
+            return false;
+        }
+
+        //check if the user has custom configurations for starting storybook
+
+        const packageJSON = JSON.parse(configData.toString());
+        const storybookScript = packageJSON.scripts.storybook;
+        const retrievedScriptArray = storybookScript.split(' ');
+        return retrievedScriptArray
+    }
+
 }
+
 
 export default ProcessService;
